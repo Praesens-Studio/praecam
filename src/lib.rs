ebe5bbc990b1b38f37450e340fef40eae1904fc8
Daniel John <danieljohnbyns@gmail.com>
2026-03-04 17:21:51 +0800

    Refactor camera struct and enhance camera listing functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,34 @@
 use nokhwa;
 
-enum CameraType {
+pub enum CameraType {
 	Webcam,
 	DSLR, // Not supported yet, but will be in the future.
 }
 
-struct Camera {
-	id: String,
-	name: String,
-	camera_type: CameraType,
+pub struct PreacamCamera {
+	pub id: String,
+	pub name: String,
+	pub camera_type: CameraType,
+}
+
+pub fn list_cameras() -> Vec<PreacamCamera> {
+	let cameras_devices = nokhwa::query(nokhwa::utils::ApiBackend::Auto);
+	let mut cameras = Vec::new();
+	match cameras_devices {
+		Ok(devices) => {
+			for device in devices {
+				cameras.push(PreacamCamera {
+					id: device.index().to_string(),
+					name: device.human_name(),
+					camera_type: CameraType::Webcam,
+				});
+			}
+		}
+		Err(e) => {
+			eprintln!("Error listing cameras: {}", e);
+		}
+	}
+	cameras
 }
 
 #[cfg(test)]
@@ -16,7 +36,13 @@ mod tests {
 	use super::*;
 
 	#[test]
-	fn list() {}
+	fn list() {
+		let cameras = list_cameras();
+		assert!(!cameras.is_empty(), "No cameras found");
+		for camera in cameras {
+			println!("Camera ID: {}, Name: {}", camera.id, camera.name);
+		}
+	}
 
 	#[test]
 	fn start() {}
